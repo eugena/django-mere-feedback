688d682de74d91db2b0e19d92f558cf8b696602d
eugena <eugena.mihailikova@gmail.com>
2015-10-08 12:00:22 +0500

    package

--- feedback/views.py
+++ feedback/views.py
@@ -1,5 +1,5 @@
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -7,6 +7,32 @@ from django.contrib.auth.models import User
 from django.views.generic import CreateView
 from django.utils.translation import ugettext as _
 import forms
+
+
+class AjaxableResponseMixin(object):
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        # We make sure to call the parent's form_valid() method because
+        # it might do some processing (in the case of CreateView, it will
+        # call form.save() for example).
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
 
 
 class FeedbackFormView(CreateView):
